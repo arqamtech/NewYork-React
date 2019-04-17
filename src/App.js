7ed15f64c2dd37065c8063379e5fb7830a88b59f
Mohammed Arqam <arqam@ebaniadvertising.in>
2019-04-17 20:37:43 +0530

    axioms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+const axios = require('axios')
 
 const API = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=3UiqpoK1IbAXFeEGbJsfwRoc6zilXJhp';
 const DEFAULT_QUERY = '';
@@ -10,27 +11,64 @@ class App extends Component {
 
     this.state = {
       posts: [],
-      isLoading: false,
       error: null,
     };
+    this.GetSection = this.GetSection.bind(this)
+    this.onChange = this.onChange.bind(this)
   }
-  componentDidMount() {
-    fetch(API + DEFAULT_QUERY)
-      .then(response => response.json())
-      .then(data =>
-        // console.log(data.results),
-        this.setState({ posts: data.results, isLoading: false })
+  async componentWillMount() {
+    let res = await axios.get(API)
+    console.log(res.data.results)
+    if (res) {
+      this.setState({ posts: res.data.results })
+    }
 
-      );
+    // fetch(API + DEFAULT_QUERY)
+    //   .then(response => response.json())
+    //   .then(data =>
+    //     // console.log(data.results)
+    //     this.setState({ posts: data.results, isLoading: false })
+
+    //   );
+  }
+
+  GetSection() {
+    let p = this.state.posts
+
+  }
+  onChange(e) {
+    let myArr = []
+    console.log(e.target.value, "akjdshadhkjks")
+    let myposts = this.state.posts
+    for (let obj of myposts) {
+
+      if (obj.section == e.target.value) {
+        myArr.push(obj);
+      }
+
+    }
+    console.log(myArr,"myArrmyArrmyArrmyArrmyArrmyArr")
+    this.setState({
+      posts: myArr
+    })
+
   }
 
   render() {
-    const { posts, isLoading } = this.state;
-    if (isLoading) {
-      return <p>Loading ...</p>;
-    }
+    const { posts } = this.state;
+    console.log(posts)
     return (
       <div className="topDiv" >
+        <select onChange={(e) => this.onChange(e)}>
+          {
+            posts.map((section, i) => {
+              return (<option key={i} value={section.section}>{section.section}</option>)
+            })
+          }
+        </select>
+
+
+
         {posts.map(post =>
           <div key={post.title}>
             <a href={post.short_url} target="_blank" rel="noopener noreferrer">
