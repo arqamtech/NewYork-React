51d3e423fa3aaa0f4a54f213dedf5d1635269366
Mohammed Arqam <arqam@ebaniadvertising.in>
2019-04-18 14:30:52 +0530

    Sorting and bootstrap done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import Moment from 'moment';
+
+
 import './App.css';
 const axios = require('axios')
-
 const API = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=3UiqpoK1IbAXFeEGbJsfwRoc6zilXJhp';
 
 class App extends Component {
@@ -14,7 +16,8 @@ class App extends Component {
       posts: [],
       sections: [],
     };
-    this.onChange = this.onChange.bind(this)
+    this.filter = this.filter.bind(this)
+    this.sort = this.sort.bind(this)
 
   }
   async componentDidMount() {
@@ -24,19 +27,60 @@ class App extends Component {
     }
   }
 
-  onChange(e) {
+  filter(e) {
     let myArr = [];
     let myposts = null;
     myposts = this.state.allposts;
-    for (let obj of myposts) {
-
-      if (obj.section === e.target.value) {
-        myArr.push(obj);
+    if (e.target.value === "all") {
+      myArr = this.state.allposts;
+    } else {
+      for (let obj of myposts) {
+        if (obj.section === e.target.value) {
+          myArr.push(obj);
+        }
       }
 
     }
     this.setState({
       posts: myArr
+    })
+
+  }
+  sort(e) {
+
+    let myposts = null;
+    myposts = this.state.posts;
+
+
+    let choice = e.target.value;
+    switch (choice) {
+      case "default":
+        myposts = this.state.posts;
+      case "createdDate":
+        myposts.sort(function (a, b) {
+          return new Date(b.created_date) - new Date(a.created_date);
+        });
+
+        break;
+      case "publishedDate":
+        myposts.sort(function (a, b) {
+          return new Date(b.published_date) - new Date(a.published_date);
+        });
+        break;
+      case "updatedDate":
+        myposts.sort(function (a, b) {
+          return new Date(b.updated_date) - new Date(a.updated_date);
+        });
+        break;
+    }
+
+
+
+
+    myposts.reverse();
+
+    this.setState({
+      posts: myposts
     })
 
   }
@@ -48,41 +92,86 @@ class App extends Component {
 
     let sortedSections = [...new Map(sections.map(o => [o.section, o])).values()];
 
+
+
+
     return (
-
-
       <div className="topDiv" >
+        <div className="row justify-content-center">
+          <div className="col-10 col-md-6  col-sm-10 col-xl-4">
 
-        <div className="form-group">
-          <label htmlFor="sectn">Select Section :</label>
-          <select onChange={(e) => this.onChange(e)}>
-            {
-              sortedSections.map((s, i) => {
-                return (<option key={i} value={s.section}>{s.section}</option>)
-              })
-            }
-          </select>
-        </div>
-        {posts.map(post =>
-          <div key={post.title}>
-            <a href={post.short_url} target="_blank" rel="noopener noreferrer">
-              <h4>
-                {post.title}
-              </h4>
-            </a>
-            <h5>{post.section}</h5>
-            <p>{post.abstract}</p>
-            {post.des_facet.map(des =>
-              <p key={des}>{des}</p>
+
+
+            <div className="form-group row justify-content-center">
+              <label htmlFor="sectn">Filter by :</label>
+              <select className="form-control" id="sectn" onChange={(e) => this.filter(e)}>
+                <option key="all" value="all">All</option>
+                {
+                  sortedSections.map((s, i) => {
+                    return (<option key={i} value={s.section}>{s.section}</option>)
+                  })
+                }
+              </select>
+            </div>
+
+            <div className="form-group row justify-content-center">
+              <label htmlFor="sectn">Sort by :</label>
+
+              <select className="form-control" id="sectn" onChange={(e) => this.sort(e)}>
+                <option key="default" value="default">Default</option>
+                <option key="createdDate" value="createdDate">Created Date</option>
+                <option key="publishedDate" value="publishedDate">Published Date</option>
+                <option key="updatedDate" value="updatedDate">Updated Date</option>
+              </select>
+            </div>
+
+
+            {posts.map(post =>
+              <div className="card m-3" key={post.title}>
+                <div className="card-body">
+
+                  <a href={post.short_url} target="_blank" rel="noopener noreferrer">
+                    <h5 className="card-title">
+                      {post.title}
+                    </h5>
+                  </a>
+
+
+                  {/* <h5>{post.multimedia}</h5> */}
+                  <h6 className="card-subtitle mb-2 text-muted">{post.abstract}</h6>
+                  {/* <p class="font-weight-bold">{post.section}</p> */}
+
+                  {post.des_facet.map(des =>
+                    <p key={des} className="desc" >{des}</p>
+                  )}
+
+
+                  {/* {post.multimedia.map((s, i) =>
+                  <img className="w-50" src={s.url} />
+                )} */}
+
+                  {/* {post.multimedia[4]} */}
+
+
+                  <p>{Moment(post.created_date).format('MMMM Do YYYY, h:mm:ss a')}</p>
+                  <p>{Moment(post.published_date).format('MMMM Do YYYY, h:mm:ss a')}</p>
+                  <p>{Moment(post.updated_date).format('MMMM Do YYYY, h:mm:ss a')}</p>
+                </div>
+
+                <div className="card-body"  >
+                  <a href={post.short_url} rel="noopener noreferrer" target="_blank" >
+                    <button type="button" className="btn btn-primary float-right">Read More</button>
+                  </a>
+
+                </div>
+
+
+              </div>
             )}
 
-
-
-            <p>{post.created_date}</p>
-            <p>{post.updated_date}</p>
-            <p>{post.published_date}</p>
           </div>
-        )}
+        </div>
+
       </div>
     );
   }
